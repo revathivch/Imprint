6734cb27310d64f59edadfbe15dedffc082aad00
Revathi Vipinachandran <revathivch@gmail.com>
2024-04-10 10:59:51 -0400

    Added overview js and css files

---
+++ src/pages/dashboard/Overview.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Overview.css';
+
+const Overview = () => {
+  // You'd typically fetch these values from a server
+  const raisedThisMonth = 5405;
+  const availablePayout = 2218;
+  const newSupporters = 72;
+  const newFollowers = 297;
+
+  return (
+    <div className='overview'>
+      <div>
+        <h2>Raised This Month</h2>
+        <p>${raisedThisMonth}</p>
+      </div>
+      <div>
+        <h2>Available Payout</h2>
+        <p>${availablePayout}</p>
+      </div>
+      <div>
+        <h2>New Supporters</h2>
+        <p>{newSupporters}</p>
+      </div>
+      <div>
+        <h2>New Followers</h2>
+        <p>{newFollowers}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Overview;
