08cf66737634ec20446ce8c36581b64d71f9772d
Revathi Vipinachandran <revathivch@gmail.com>
2024-04-10 10:51:55 -0400

    Created folder structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'; // This imports the main stylesheet for the app
+import Dashboard from './Dashboard'; // This imports the Dashboard component
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
